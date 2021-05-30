6f307be0193e5350efccc5a99ce4069374ccb220
Phil Yates <phil.yates@me.com>
2021-05-30 11:04:33 +0100

    Building first draft of the main screen

--- what-to-wear-today/ContentView.swift
+++ what-to-wear-today/ContentView.swift
@@ -9,13 +9,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world test")
+        ScrollView(.vertical, showsIndicators: false, content: {
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Rugeley")
+                    .font(.title)
+                    .bold()
+                VStack(spacing: 16) {
+                    HStack() {
+                        Image("advicons.umbrella")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: nil, alignment: .center)
+                            
+                        VStack(alignment: .leading) {
+                            Text("Title")
+                                .font(.headline)
+                            Text("Sub Title")
+                                .font(.subheadline)
+                                .foregroundColor(Color("colors.subTitle"))
+                        }
+                        Spacer()
+                    }
+                    
+                    HStack() {
+                        Image("advicons.umbrella")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: nil, alignment: .center)
+                            
+                        VStack(alignment: .leading) {
+                            Text("Title")
+                                .font(.headline)
+                            Text("Sub Title")
+                                .font(.subheadline)
+                                .foregroundColor(Color("colors.subTitle"))
+                        }
+                        Spacer()
+                    }
+                    
+                    HStack() {
+                        Image("advicons.umbrella")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: nil, alignment: .center)
+                            
+                        VStack(alignment: .leading) {
+                            Text("Title")
+                                .font(.headline)
+                            Text("Sub Title")
+                                .font(.subheadline)
+                                .foregroundColor(Color("colors.subTitle"))
+                        }
+                        Spacer()
+                    }
+                }
+                .groupStyle()
+                
+                VStack(spacing: 16) {
+                    HStack(spacing: 16) {
+                        Text("11:00")
+                            .font(.title3)
+                            .bold()
+                        Image(systemName: "cloud.sun.rain.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: nil, alignment: .center)
+                        Spacer()
+                    }
+                }.groupStyle()
+            }
+            .frame(
+              minWidth: 0,
+              maxWidth: .infinity,
+              minHeight: 0,
+              maxHeight: .infinity,
+              alignment: .topLeading
+            )
             .padding()
+        })
+        .foregroundColor(Color.white)
+        .background(Color("colors.background"))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func groupStyle() -> some View {
+        self.padding()
+            .background(Color("colors.groupBg"))
+            .cornerRadius(9.0)
     }
 }
